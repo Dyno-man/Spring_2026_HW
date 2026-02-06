b518a9350b6f75a5e2f6a4abc157ea8e6f8b7d68
Dyno-man <grantversluis@gmail.com>
2026-02-06 13:08:32 -0500

    NLP assignmnet fixed

--- txt_normilzation.py
+++ txt_normilzation.py
@@ -3,14 +3,14 @@ lowercasing, removing punctuation, tokenization, removing stop words, and stemmi
 """
 import re
 import string
-from nltk.corpus import stopwords
 import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 nltk.download('stopwords')
 
-
 stop_words = set(stopwords.words('english'))
-
+stemmer = PorterStemmer()
 
 doc1 = "She sells sea shells by the seashore."
 doc2 = "How much wood would a woodchuck chuck, if a woodchuck could chuck wood?"
@@ -19,22 +19,57 @@ doc3 = "A journey of a thousand miles begins with a single step."
 docs = [doc1, doc2, doc3]
 
 def normalize(sentence):
-    #Lowering sentences
+    # Lowercase
     sentence = sentence.lower()
 
-    #Define all punctuation
+    # Remove punctuation
     punc = f'[{re.escape(string.punctuation)}]'
-
-    #Remove all punctuation
     sentence = re.sub(punc, "", sentence)
+    
+    # Tokenize
+    tokens = sentence.split()
 
-    sentence = [word for word in sentence if word not in stop_words]
+    # Remove stop words
+    tokens = [word for word in tokens if word not in stop_words]
+    # Stemming
+    tokens = [stemmer.stem(word) for word in tokens]
+    return tokens
 
-    #Tokenize the sentence
-    # sentence = sentence.split()
 
-    return sentence
+def vocab(sentence):
+    seen = set()
+    out = []
+    for word in sentence:
+        if word not in seen:
+            seen.add(word)
+            out.append(word)
+    return out
+
+tokenized_docs = []
+vocab_docs = []
 
 
-for stuff in docs:
-    print(normalize(stuff))
+# Normalize Docs
+for doc in docs:
+    tokenized_docs.append(normalize(doc))
+
+# Find Vocab for docs
+for doc in tokenized_docs:
+    vocab_docs.append(vocab(doc))
+
+num = 1 
+for token in tokenized_docs:
+    print(f"Amount of Tokens for doc {num}: ", end='')
+    print(len(token))
+    num +=1
+
+print()
+
+num = 1 
+for token in vocab_docs:
+    print(f"Amount of vocab for doc {num}: ", end='')
+    print(len(token))
+    num +=1
+        
+print(tokenized_docs)
+print(vocab_docs)
